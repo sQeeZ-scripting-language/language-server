57ce307efaeed4863d79becea423cef7bee558a6
marcelnoehre <marcelnoehre@web.de>
2024-09-30 17:39:20 +0200

    recognizer .sqz files

---
+++ client/src/extension.ts
@@ -0,0 +1,48 @@
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	const serverModule = context.asAbsolutePath(
+		path.join('server', 'out', 'server.js')
+	);
+
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+		}
+	};
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'plaintext', pattern: '**/*.sqz' }],
+		synchronize: {
+			fileEvents: workspace.createFileSystemWatcher('**/*.sqz')
+		}
+	};
+
+	client = new LanguageClient(
+		'languageServer',
+		'sQeeZ Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
